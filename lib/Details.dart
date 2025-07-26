880418b417f7cae2673c551a16f5c4e06c6d34f1
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-07-25 21:54:36 -0400

    :sparkles: feat: detalhes da cotação selecionada

--- lib/Details.dart
+++ lib/Details.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './Quotes.dart';
+import 'QuotesProvider.dart';
 
 class Details extends StatelessWidget {
-  const Details({super.key});
+
+  final String keyQuote;
+  final double valueQuote;
+
+  const Details({super.key, required this.keyQuote, required this.valueQuote});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Destalhes')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Voltar'),
+    final QuotesProvider quotesProvider =
+    Provider.of<QuotesProvider>(context);
+    final Quotes? quotes = quotesProvider.quotesData;
+
+    if (quotes != null) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Detalhes - Cotação ${keyQuote}')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Cotação financeira: $keyQuote"),
+              Text('Valor: ${valueQuote.toStringAsFixed(4)}'),
+              Text('Última atualização: ${quotes.lastTimeUpdate}'),
+              Text('Próxima atualização: ${quotes.nextTimeUpdate}'),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
+    return const Text('Erro ao mostrar os detalhes da cotação!');
   }
 }
